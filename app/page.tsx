2b24f5974cc2a97c440240f6ec380ed1e6acbe0b
goku-raj <anshuraj1479@gmail.com>
2023-09-19 15:10:55 +0530

    environment setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,12 @@
-import Image from 'next/image'
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-    <p className='text-2xl text-red-500'>Hello LMS App</p>
+    <div>
+      <p className='text-2xl text-red-500'>Hello LMS App</p>
+      <Button variant="destructive">
+        click me
+      </Button>
+    </div>
   )
 }
