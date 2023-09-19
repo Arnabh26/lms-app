521e6932af59bd3cd9087334bb9e2d3ff4432ca4
goku-raj <anshuraj1479@gmail.com>
2023-09-19 17:05:43 +0530

    Authentication using Clerk

--- app/(auth)/(routes)/sign-in/page.tsx
+++
@@ -1,9 +0,0 @@
-const SignInpage = () => {
-    return ( 
-        <div>
-            This is an unprotected page
-        </div>
-     );
-}
- 
-export default SignInpage;
--- app/(dashboard)/(routes)/page.tsx
+++ app/(dashboard)/(routes)/page.tsx
@@ -1,9 +1,13 @@
+import { UserButton } from "@clerk/nextjs"
+
 
 
 export default function Home() {
   return (
     <div>
-      <p>This is a protected Page</p>
+      <UserButton
+      afterSignOutUrl="/"
+      />
     </div>
   )
 }
