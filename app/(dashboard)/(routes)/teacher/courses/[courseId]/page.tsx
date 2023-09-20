94e51dcf191a77f5c8e1741700ef5cecef3ac074
goku-raj <anshuraj1479@gmail.com>
2023-09-20 23:14:27 +0530

    course title form

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
@@ -4,6 +4,7 @@ import { auth } from "@clerk/nextjs";
 import { LayoutDashboard } from "lucide-react";
 import { redirect } from "next/navigation";
 import { boolean } from "zod";
+import TitleForm from "./_components/title-form";
 
 
 const CourseIdPage = async ({
@@ -60,6 +61,10 @@ const CourseIdPage = async ({
                             Customize your course
                         </h2>
                     </div>
+                    <TitleForm 
+                        initialData = {course}
+                        courseId = {course.id}
+                    />
                 </div>
             </div>
         </div>
