db66d81fc156b2808ef87403ea97b62f652b3640
goku-raj <anshuraj1479@gmail.com>
2023-09-29 14:10:01 +0530

    chapter video

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/chapters/[chapterId]/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/chapters/[chapterId]/page.tsx
@@ -1,12 +1,13 @@
 import { IconBadge } from "@/components/icons-badge";
 import { db } from "@/lib/db";
 import { auth } from "@clerk/nextjs";
-import { ArrowLeft, Eye, LayoutDashboard } from "lucide-react";
+import { ArrowLeft, Eye, LayoutDashboard, Video } from "lucide-react";
 import Link from "next/link";
 import { redirect } from "next/navigation";
 import ChapterTitleForm from "./_components/chapter-title-form";
 import ChapterDescriptionForm from "./_components/chapter-description-form";
 import ChapterAccessForm from "./_components/chapter-access-form";
+import ChapterVideo from "./_components/chapter-video-form";
 
 const ChapterIdPage = async ({
     params
@@ -100,6 +101,19 @@ const ChapterIdPage = async ({
                             chapterId={params.chapterId}
                         />
                     </div>
+                </div>
+                <div>
+                    <div className="flex items-center gap-x-2">
+                        <IconBadge icon={Video}/>
+                        <h2 className="text-xl">
+                            Add a video
+                        </h2>
+                    </div>
+                    <ChapterVideo
+                        initialData={chapter}
+                        chapterId={params.chapterId}
+                        courseId={params.courseId}
+                    />
                 </div>
             </div>
         </div>
